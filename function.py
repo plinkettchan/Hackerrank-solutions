3a94a5108715ba9f0d680c92d6ef07079148ac0f
Parvathy Subramanian <64288351+plinkettchan@users.noreply.github.com>
2021-05-22 19:09:01 -0400

    Create function.py

---
+++ function.py
@@ -0,0 +1,14 @@
+def is_leap(year):
+    leap = False
+    if 1900<=year<=10**5:
+        if (year%4==0 and year%100!=0) or (year%100==0 and year%400==0):
+            return True
+        else:
+            return False
+    else:
+        return False
+    # Write your logic here
+    
+    return leap
+
+year = int(input())
